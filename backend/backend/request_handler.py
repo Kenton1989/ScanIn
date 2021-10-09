dc7ab080076585053b993dd1aa5f71621e64ebc0
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-10-09 14:42:33 +0800

    Update request_handler.py

--- backend/backend/request_handler.py
+++ backend/backend/request_handler.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+import json
 
-def handle_cz3002(request):
-    return HttpResponse(b'It works (CZ3002)')
+def handle_cz3002(request: HttpRequest):
+    body = request.body
+    if len(body) < 2: body = '{"test":"3002"}'
+    req = json.loads(body)
+    
+    return HttpResponse(bytes('It works (CZ3002) \n' + json.dumps(req), encoding='utf8'))
 
 
-def handle_home(request):
+def handle_home(request: HttpRequest):
     return HttpResponse(b'It works')
