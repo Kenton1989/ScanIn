4494951d3daec3828805aed8d9d568190200833d
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-10-09 13:04:11 +0800

    Update request_handler.py

--- backend/backend/request_handler.py
+++ backend/backend/request_handler.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 def handle_cz3002(request):
     return HttpResponse(b'It works (CZ3002)')
 
-    
+
 def handle_home(request):
-    return HttpResponse(b'It works (Home)')
+    return HttpResponse(b'It works')
