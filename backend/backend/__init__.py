dab0935222bf37c80b32d2b1ddeea51fd53d9864
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-10-10 02:51:03 +0800

    Update __init__.py

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -4,4 +4,3 @@ import sys
 curDir = os.path.dirname(os.path.realpath(__file__))
 if curDir not in sys.path:  # add current dir to paths
     sys.path.append(curDir)
-    print("added", curDir, "into search path")
