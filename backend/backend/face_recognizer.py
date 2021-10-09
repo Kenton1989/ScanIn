76317405f04f35f97512854a502072f1bf14b502
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-10-09 23:50:18 +0800

    Create face_recognizer.py

---
+++ backend/backend/face_recognizer.py
@@ -0,0 +1,87 @@
+import face_recognition
+import numpy
+from DatabaseAccessor import DatabaseAccessor
+from typing import List
+
+def _checkLen(vects, labels):
+    if len(vects) != len(labels):
+        print("WARNING: length of vector does not match length of labels")
+        print("recognizer returned without modification, or null recognizer created")
+        return False
+    return True
+
+
+class FaceRecognizer:
+    def __init__(self,  db_accessor: DatabaseAccessor):
+        # TODO: use accessor to get the initialization data
+        init_vectors: List[numpy.ndarray] = []
+        init_labels: List[str] = []
+
+        if not _checkLen(init_vectors, init_labels):
+            self.vectors = None
+            self.labels = None
+            self.label_dict = None
+            return
+        if len(init_vectors[0]) != 128 and not isinstance(init_vectors[0], int):
+            init_vectors = [face_recognition.face_encodings(idx) for idx in range(len(init_vectors))]
+        self.vectors = init_vectors
+        self.labels = init_labels
+        tempDict = {}
+        for idx in range(len(init_vectors)):
+            tempDict[init_labels[idx]] = init_vectors[idx]
+        self.label_dict = tempDict
+
+    def recognize_face(self, image: numpy.ndarray):
+        unknown_encoding = face_recognition.face_encodings(image)
+        results = face_recognition.compare_faces(self.vectors, unknown_encoding, tolerance=Hyperparams.TOLERANCE)
+        result_labels = []
+        for idx in range(len(results)):
+            if results[idx]:
+                result_labels.append(self.labels[idx])
+        return result_labels
+
+    def register_face(self, pid: int, image: numpy.ndarray):
+        result = face_recognition.face_encodings(image)
+        self._send_db_register(pid, result)
+
+    def _send_db_register(self, pid: int, encoding: numpy.ndarray):
+        pass
+
+    def get_vectors(self):
+        return self.vectors
+
+    def get_labels(self):
+        return self.labels
+
+    def get_label_dict(self):
+        return self.label_dict
+
+    def reset(self, new_vects: List[numpy.ndarray], new_labels: List[str]):
+        if _checkLen(new_vects, new_labels):
+            for idx in range(len(new_vects)):
+                if len(new_vects[idx]) != 128:
+                    print("WARNING: length of vector[", idx, "] is not 128.")
+                    print("you must make sure that every element in vector has the dimension of 128 for recognition.")
+                    print("recognizer returned without modification")
+                    return False
+            if len(new_vects[0]) != 128:
+                new_vects = [face_recognition.face_encodings(idx) for idx in range(len(new_vects))]
+            self.vectors = new_vects
+            self.labels = new_labels
+            tempDict = {}
+            for idx in range(len(new_vects)):
+                tempDict[new_labels[idx]] = new_vects[idx]
+            self.label_dict = tempDict
+            print("recognizer successfully updated.")
+            return True
+        return False
+
+    def reset_from_db(self, new_vects: List[numpy.ndarray], new_labels: List[str]):
+        pass
+
+    def _get_db_vectors(self):
+        pass
+
+
+class Hyperparams:
+    TOLERANCE = 0.6
