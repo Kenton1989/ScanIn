453333f40450fa9d13417c8ba1d2b9baf214d0b6
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-10-10 18:56:29 +0800

    Update request_handler.py

--- backend/backend/request_handler.py
+++ backend/backend/request_handler.py
@@ -18,7 +18,7 @@ def _success_response(returnVal):
     return response
 
 
-def _failed_response(errMsg='invalid request.'):
+def _failed_response(errMsg='invalid request'):
     assert isinstance(errMsg, str)
     res = {
         'success': False,
@@ -30,14 +30,20 @@ def _failed_response(errMsg='invalid request.'):
 
 
 def _default_handler(auth, param):
-    return _failed_response('Unknown operation.')
+    return _failed_response('unknown operation')
+
 
 def _valid_auth_format(auth):
-    if auth == None: return True
-    if not isinstance(auth, dict): return False
-    if len(auth) != 2: return False
-    if not isinstance(auth['username'], str): return False
-    if not isinstance(auth['password'], str): return False
+    if auth == None:
+        return True
+    if not isinstance(auth, dict):
+        return False
+    if len(auth) != 2:
+        return False
+    if not isinstance(auth['username'], str):
+        return False
+    if not isinstance(auth['password'], str):
+        return False
     return True
 
 
@@ -59,12 +65,10 @@ def handle_cz3002(request: HttpRequest):
     operation = req['operation']
     auth = req['auth']
     param = req['param']
-    
-    try:
-        assert isinstance(operation, str)
-        assert _valid_auth_format(auth)
-        assert isinstance(param, dict)
-    except:
+
+    if not (isinstance(operation, str) and
+            _valid_auth_format(auth) and
+            isinstance(param, dict)):
         log.error('Invlid request structure')
         return _failed_response()
 
