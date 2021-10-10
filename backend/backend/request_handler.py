8259efe2295b485f160dcaa9eb09f4b2b4c9815b
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-10-10 18:05:29 +0800

    Update request_handler.py

--- backend/backend/request_handler.py
+++ backend/backend/request_handler.py
@@ -1,13 +1,52 @@
 from django.http import HttpResponse, HttpRequest, JsonResponse
 import json
 from face_recognizer import FaceRecognizer
+import logging
+
+log = logging.getLogger('Request Handler')
+
+def _success_response(returnVal):
+    assert isinstance(returnVal, dict)
+    res = {
+        'success': True,
+        'details': "",
+        'return': returnVal,
+    }
+    response = JsonResponse(res)
+    return response
+
+def _failed_response(errMsg):
+    assert isinstance(errMsg, str)
+    res = {
+        'success': False,
+        'details': errMsg,
+        'return': {},
+    }
+    response = JsonResponse(res)
+    return response
+
+def _default_handler(auth, param):
+    return _failed_response('Unknown operation.')
+
+
+OP_HANDLER = {}
 
 def handle_cz3002(request: HttpRequest):
-    body = request.body
-    if len(body) < 2: body = '{"test":"3002"}'
-    req = json.loads(body)
-    req["msg"] = "It works!"
-    return JsonResponse(req)
+    if request.method != 'POST':
+        log.debug('Only POST method is used')
+
+    try:
+        req = json.loads(request.body)
+    except json.decoder.JSONDecodeError:
+        log.error('Cannot parse the request into JSON object.')
+
+    operation = req['operation']
+    auth = req['auth']
+    param = req['param']
+
+    handler = OP_HANDLER.get(operation, _default_handler)
+
+    return handler(auth, operation)
 
 
 def handle_home(request: HttpRequest):
