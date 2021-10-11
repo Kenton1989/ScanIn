e3017c42688035cffa83a8d720f519458b1303c9
johnleekj <43510466+johnleekj@users.noreply.github.com>
2021-10-11 10:57:25 +0800

    added login page

---
+++ android_john/app/src/main/java/com/example/scanin/Register_Fragment.java
@@ -0,0 +1,36 @@
+package com.example.scanin;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Register_Fragment extends Fragment {
+
+    private RegisterViewModel mViewModel;
+
+    public static Register_Fragment newInstance() {
+        return new Register_Fragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.register__fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
