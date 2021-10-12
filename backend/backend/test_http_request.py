5282d494bf0c43264cb9190094ca7cc338487088
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-10-12 16:46:23 +0800

    Update test_http_request.py

--- backend/backend/test_http_request.py
+++ backend/backend/test_http_request.py
@@ -18,14 +18,70 @@ def send(opName, params={}, auth=None):
     data_byte = json.dumps(data).encode('utf8')
     print('Sending', data, 'to', URL)
     req = Request(URL, data_byte, headers=header, method='POST')
-    
+
     response = urlopen(req)
-    
+
     print('Loading response')
     reply = json.load(response)
 
     return reply
 
 
+def test_login():
+    print(send(
+        opName='login',
+        params={
+            'username': 'admin',
+            'password': 'password',
+        },
+        auth=None
+    ))
+
+
+def test_get_history():
+    print(send(
+        opName='get_valid_history_param',
+        params={},
+        auth={
+            'username': 'admin',
+            'hashed_password': '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
+        }
+    ))
+
+
+def test_get_history():
+    param = {
+        'pid': 'admin',
+        'sid': 4,
+        'beg_time': None,
+        'end_time': None,
+        'max_num': 10,
+    }
+    print(send(
+        opName='get_history',
+        params={},
+        auth={
+            'username': 'admin',
+            'hashed_password': '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
+        }
+    ))
+
+
+def test_get_last_history():
+    print(send(
+        opName='get_last_history',
+        params={
+            'sid': 4
+        },
+        auth={
+            'username': 'admin',
+            'hashed_password': '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
+        }
+    ))
+
+
+
 if __name__ == '__main__':
-    print(send('get_something', {}, {'username': 'aba', 'password': 'hey'}))
+    # test_login()
+    # test_get_history_param()
+    test_get_last_history()
