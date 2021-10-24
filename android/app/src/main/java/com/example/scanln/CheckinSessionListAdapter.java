26762b975321578b5722bc604ec08adc07c784b8
Wang Annan <66814452+AnnanWangDaniel@users.noreply.github.com>
2021-10-24 20:02:59 +0800

    Update CheckinSessionListAdapter.java

--- android/app/src/main/java/com/example/scanln/CheckinSessionListAdapter.java
+++ android/app/src/main/java/com/example/scanln/CheckinSessionListAdapter.java
@@ -5,8 +5,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
-
 import com.example.scanln.model.History;
 import com.example.scanln.model.SessionBrief;
 
